97e808820089d66f7768e610c7a47b25f38769a8
Abdelouafi Tahiri <abdlwafi943@gmail.com>
2024-10-28 18:31:30 +0100

    update

--- dashboard_app/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-
-def user_context(request):
-    user = request.user
-    return {
-        'username': user.username if user.is_authenticated else 'Invité',
-        'role': 'Administrateur' if user.is_staff else 'Utilisateur',
-    }
